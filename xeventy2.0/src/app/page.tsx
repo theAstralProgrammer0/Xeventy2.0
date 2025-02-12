5fa05e9522c2d66b84d663ed49ef73680bdbf901
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-12 04:21:29 +0100

    Starting point of next.js app

---
+++ xeventy2.0/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ height: "100vh", padding: "20px", backgroundColor: "#232323", color: "white" }}>
+        <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>Homepage</h1>
+        <p style={{ fontSize: "18px", marginBottom: "30px" }}>
+          
+      );
+}
