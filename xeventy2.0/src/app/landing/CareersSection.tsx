0016470aef93ec1b6c3965d1c3f3cef19cdecf96
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-13 15:19:12 +0100

    Setup and ready for editing page

---
+++ xeventy2.0/src/app/landing/CareersSection.tsx
@@ -0,0 +1,15 @@
+import { Button, Img, Text } from "../../components";
+import React from "react";
+
+export default function CareersSection() {
+  return (
+    <>
+      {/* careers section */}
+      <div className="mt-[154px] flex flex-col items-center">
+        <div className="container-xs flex flex-col gap-[60px] px-14 md:px-5 sm:gap-[30px]">
+          <div className="mr-1 flex flex-col items-center gap-6 md:mr-0">
+            <Button
+              color="black_900_01"
+              size="xs"
+              shape="round"
+              className="min-w-[72px] rounded-[10px] px-1.5 font-merriweather tracking-[1.09px]"
--- xeventy2.0/src/app/layout.tsx
+++ xeventy2.0/src/app/layout.tsx
@@ -1,35 +1,22 @@
 import "@/styles/tailwind.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
-  title: "Xeventy2.0 Health Web App",
+  title: "Xeventy2.0 Health Limited",
   description: "Xeventy2.0 Health is a web app designed for heallth, telehealt, EMR, electronic health records",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
