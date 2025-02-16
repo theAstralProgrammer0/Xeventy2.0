389639c7fb47c4243c370878eef6d3e434617d05
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-16 18:24:49 +0100

    Completed CareersSection component

--- xeventy2.0/src/app/landing/CareersSection.tsx
+++ xeventy2.0/src/app/landing/CareersSection.tsx
@@ -13,3 +13,72 @@ export default function CareersSection() {
               size="xs"
               shape="round"
               className="min-w-[72px] rounded-[10px] px-1.5 font-merriweather tracking-[1.09px]"
+            >
+              Careers
+            </Button>
+            <Text
+              size="text12xl"
+              as="p"
+              className="self-stretch text-center text-[49.62px] font-normal leading-[60px] !text-black-900_01 md:text-[45px] sm:text-[39px]"
+            >
+              Join us in creating a tech engaged healthcare system in Africa
+            </Text>
+            <Text
+              size="text2xl"
+              as="p"
+              className="self-stretch text-center text-[16.54px] font-normal leading-8 !text-black-900_01"
+            >
+              At Xeventy2.0 Health Limited, we nurture talent and foster
+              innovation to drive meaningful change in digital health.  Our
+              collaborative, supportive       environment values diverse
+              perspectives and encourages creativity, leading to cutting-edge
+              solutions that improve access to care and patient outcomes
+              worldwide.  Passionate about digital health and global impact?
+              Explore our career opportunities and join our team shaping the
+              future of healthcare.
+            </Text>
+          </div>
+          <div className="relative h-[816px]">
+            <Img
+              src="img_pic3.png"
+              width={436}
+              height={652}
+              alt="Picthree"
+              className="absolute right-[-0.65px] top-[6%] m-auto h-[652px] w-[36%] rounded-[12px] object-contain"
+            />
+            <Img
+              src="img_pic2.png"
+              width={436}
+              height={652}
+              alt="Pictwo"
+              className="absolute bottom-[0.65px] left-0 right-0 mx-auto h-[652px] w-[36%] rounded-[12px] object-contain"
+            />
+            <Img
+              src="img_pic1.png"
+              width={436}
+              height={652}
+              alt="Picone"
+              className="absolute left-0 top-0 m-auto h-[652px] w-[36%] rounded-[12px] object-contain"
+            />
+          </div>
+          <Button
+            size="xl"
+            rightIcon={
+              <Img
+                src="img_link_icon.png"
+                width={28}
+                height={28}
+                alt="Link icon"
+                className="mb-1 h-[28px] w-[28px] object-contain"
+              />
+            }
+            className="ml-[172px] mr-[174px] gap-1.5 self-stretch rounded-[14px] px-[34px] md:mx-0 sm:px-5"
+          >
+            Explore careers
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
+           
