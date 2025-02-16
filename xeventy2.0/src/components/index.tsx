948b3b3ecc52745eed532cfd3872153b35c13dca
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-16 18:03:10 +0100

    Exported all primitive components in components/index.tsx

---
+++ xeventy2.0/src/components/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./Button";
+imoprt { Heading } from "./Heading";
+import { Img } from "./Img";
+import { Input } from "./Input";
+import { Text } from "./Text";
+import { TextArea } from "./TextArea";
+
+export { Button, Heading, img, Input, Text, TextArea };
+
