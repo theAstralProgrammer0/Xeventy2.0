0328c3b89f25c4b461e1d71f173ff258f00a2275
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-16 17:58:44 +0100

    Completed TextArea component

---
+++ xeventy2.0/src/components/TextArea/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+
+const shapes = {
+  round: "rounded-[10px]",
+} as const;
+
+const variants = {
+  tarOutlineBlack9003f: "!border-black-900_3f border-[0.85px] border-solid bg-white-a700",
+} as const;
+
+const sizes = {
+  xs: "h-[258px] p-3",
+} as const;
+
+type TextAreaProps = Omit<
+  React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>,
+  "size" | "prefix" | "type" | "onChange"
+> & 
+  Partial<{
+    className: string;
+    name: string;
+    placeholder: string;
+    onChange: Function;
+    shape: keyof typeof variants | null;
+    size: keyof typeof sizes;
+  }>;
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    {
+      className = "",
+      name = "",
+      placeholder = "",
+      shape,
+      size = "xs",
+      variant = "tarOutlineBlack9003f",
+      onChange,
+      ...restProps
+    },
+    ref,
+  ) => {
+    const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+      if (onChange) onChange(e?.target?.value);
+    };
+
+    return (
+      <textarea
+        ref={ref}
+        className={`${className} ${shape && shapes[shape]} ${size && sizes[size]} ${variant && variants[variant]}`}
+        name={name}
+        onChange={handleChange}
+        placeholder={placeholder}
+        {...restProps}
+      />
+    );
+  },
+);
+
+export { TextArea };
+
