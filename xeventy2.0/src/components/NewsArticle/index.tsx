e9fb43af7a7c2cac6eb6b30e084ee83dcbaeb956
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-16 16:49:32 +0100

    Completed NewsArticle component

---
+++ xeventy2.0/src/components/NewsArticle/index.tsx
@@ -0,0 +1,41 @@
+import { Text } from "./..";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  className?: string;
+  articleTitle?: ReactReactReactNode;
+  articleTimeline?: React.ReactNode;
+  articleAuthor?: React.ReactNode;
+}
+
+export default function NewsArticle({
+  articleTitle = "NAD Supplements Are the Latest Thing in Anti-Aging—Do They Work? ",
+  articleTimeline = "Just In",
+  articleAuthor = "By Raphael Akangbe, PhD",
+  ...props
+}: Props) {
+  return (
+    <div {..props} className={`${props.className} flex flex-col self-stretch gap-2 flex-1`}>
+      <Link href="https://dribbble.com/" target="_blank" rel="noreferrer" className="w-full leading-[19px]">
+        <Text
+          size="text2xl"
+          as="p"
+          className="text-[16.38px] font-medium tracking-[0.49px] !text-black-900_01 hover:underline"
+        >
+          {articleTitle}
+        </Text>
+      </Link>
+      <div className="flex items-center self-stretch">
+        <Text size="textxs" as="p" className="text-[10.73px] font-normal !text-black-900_01">
+          {articleTimeline}
+        </Text>
+        <div className="ml-2 h-[4px] w-[4px] rounded-sm bg-green-300" />
+        <Text size="textxs" as="p" className="ml-2 text-[10.73px] font-normal !text-black-900_01">
+          {articleAuthor}
+        </Text>
+      </div>
+    </div>
+  );
+}
+
