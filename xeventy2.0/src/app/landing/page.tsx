4368d4c49820659fba1eab57fe4074de5b523849
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-17 20:17:38 +0100

    Completed /src/app/landing/page.tsx It contains the metadata for SEO

---
+++ xeventy2.0/src/app/landing/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MetaData } from "next";
+import Page from ".";
+
+export const metadata: Metadata = {
+  title: "Healthcare Innovation - Empowering Providers Globally",
+  description: "Discover Xeventy2.0\\'s transformative healthcare solutions integrating technology and data-driven insights for revolutionary patient care. Explore our services, training, and expert insights.",
+  //ogTitle: '...'
+};
+
+export default function LandingPage() {
+  return <Page />;
+}
+
