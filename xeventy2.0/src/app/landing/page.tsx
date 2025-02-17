f5228f68598ddd750214f560a2e0ca55e567e4e1
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-17 20:19:33 +0100

    Modified page.tsx for ogTitle

--- xeventy2.0/src/app/landing/page.tsx
+++ xeventy2.0/src/app/landing/page.tsx
@@ -5,7 +5,7 @@ import Page from ".";
 export const metadata: Metadata = {
   title: "Healthcare Innovation - Empowering Providers Globally",
   description: "Discover Xeventy2.0\\'s transformative healthcare solutions integrating technology and data-driven insights for revolutionary patient care. Explore our services, training, and expert insights.",
-  //ogTitle: '...'
+  ogTitle: "Xeventy2.0 Health Innovation - Empoweing Providers Globally",
 };
 
 export default function LandingPage() {
