c81d90e77cffe7be5901de6b0aee77b273b0fe7b
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-03-25 20:37:15 +0100

    Modified about/page.tsx for SEO

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Metadata } from "next";
+import Page from ".";
+
+export const metadata: Metadata = {
+  title: "About Xeventy2.0 Health - Transforming Digital Healthcare",
+  description: "Discovered Xeventy2.0 Health's mission to brisge professionals and patients with transformative digital health solutions. Learn about our services, teams, and commitment to excellence in healthcare innovation.",
+};
+
+export default function AboutPage() {
+  return <Page />;
+}
+
