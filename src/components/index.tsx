aae6fac546ec6234749ddb2d6de7edcb6536054d
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-04-11 03:42:09 +0100

    Made SOME COMPONENTS an un-named import in components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-import AvatarStack from "./AvatarStack";
+import { AvatarStack } from "./AvatarStack";
 import Button from "./Button";
 import CheckBox from "./CheckBox";
 import Footer from "./Footer";
@@ -6,13 +6,13 @@ import Header from "./Header";
 import Heading from "./Heading";
 import HealthExpertiseProfile from "./HealthExpertiseProfile";
 import HealthcareRecordComponent from "./HealthcareRecordComponent";
-import Img from "./Img";
-import Input from "./Input";
-import NewsArticle from "./NewsArticle";
-import NewsletterSubscription from "./NewsletterSubscription";
-import ProjectTile from "./ProjectTile";
-import Text from "./Text";
-import TextArea from "./TextArea";
+import { Img } from "./Img";
+import { Input } from "./Input";
+import { NewsArticle } from "./NewsArticle";
+import { NewsletterSubscription } from "./NewsletterSubscription";
+import { ProjectTile } from "./ProjectTile";
+import { Text } from "./Text";
+import { TextArea } from "./TextArea";
 
 export { AvatarStack, Button, CheckBox, Footer, Header, Heading, HealthExpertiseProfile, HealthcareRecordComponent, Img, Input, NewsArticle, NewsletterSubscription, ProjectTile, Text, TextArea };
 
