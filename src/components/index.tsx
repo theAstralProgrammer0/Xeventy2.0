1ac1c0bc40425209d931d42e4fdd2e015dac49e6
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-04-11 03:40:17 +0100

    Made ALL COMPONENTS an un-named import in components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,18 +1,18 @@
-import { AvatarStack } from "./AvatarStack";
-import { Button } from "./Button";
-import { CheckBox } from "./CheckBox";
+import AvatarStack from "./AvatarStack";
+import Button from "./Button";
+import CheckBox from "./CheckBox";
 import Footer from "./Footer";
 import Header from "./Header";
-import { Heading } from "./Heading";
-import { HealthExpertiseProfile } from "./HealthExpertiseProfile";
-import { HealthcareRecordComponent } from "./HealthcareRecordComponent";
-import { Img } from "./Img";
-import { Input } from "./Input";
-import { NewsArticle } from "./NewsArticle";
-import { NewsletterSubscription } from "./NewsletterSubscription";
-import { ProjectTile } from "./ProjectTile";
-import { Text } from "./Text";
-import { TextArea } from "./TextArea";
+import Heading from "./Heading";
+import HealthExpertiseProfile from "./HealthExpertiseProfile";
+import HealthcareRecordComponent from "./HealthcareRecordComponent";
+import Img from "./Img";
+import Input from "./Input";
+import NewsArticle from "./NewsArticle";
+import NewsletterSubscription from "./NewsletterSubscription";
+import ProjectTile from "./ProjectTile";
+import Text from "./Text";
+import TextArea from "./TextArea";
 
 export { AvatarStack, Button, CheckBox, Footer, Header, Heading, HealthExpertiseProfile, HealthcareRecordComponent, Img, Input, NewsArticle, NewsletterSubscription, ProjectTile, Text, TextArea };
 
